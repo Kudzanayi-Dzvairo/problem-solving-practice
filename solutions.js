ea57082b22aad8acb0c72339366c8fdc75cb9b2a
Kudzanayi Dzvairo <kdzvairo@gmail.com>
2019-06-28 14:37:05 -0400

    frequecy counter

--- solutions.js
+++ solutions.js
@@ -51,3 +51,16 @@ function charCount(str){
     return result
 }
 
+//refactor 
+
+function charCount2(srt){
+    var obj = {}
+    for (var char of str) {
+        char = char.toLowerCase();
+        if(/[a-z0-9]/.test(char)){
+            obj[char] = ++obj[char] || 1
+        }
+    }
+    return obj
+}
+
